8456319f6d206fcfb37e701dda3bd7d6884e665c
bryanmccarthy86 <bryanmccarthy86@gmail.com>
2025-06-07 19:08:13 -0400

    C: Completed report chapter 3 lesson 1

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from stats import get_num_words, count_letters
+
+def create_count(book):
+    new_dict = {}
+  #  with open(f"books/{book}") as f:
+
+
 def main(book):
     with open(f"books/{book}") as f:
         get_book_text = f.read()
-        print (get_book_text)
+        #print (get_book_text)
+        lower_case = get_book_text.lower()
+        #print(lower_case)
 
-main("frankenstein.txt")
+        # get_book_text.split()
+
+
+
+get_num_words("frankenstein.txt")
+count_letters("frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,27 @@
+def get_num_words(book):
+    with open(f"books/{book}") as f:
+        get_book_text = f.read()
+#        print (get_book_text)
+        book_array = get_book_text.split()
+        num_of_words = len(book_array)
+        print(f"Found {num_of_words} total words")
+
+def count_letters(book):
+    with open(f"books/{book}") as f:
+        get_book_text = f.read()
+        lower_text = get_book_text.lower()
+        characters = list(set(lower_text))
+        #print(lower_text)
+        dict = {}
+        for letters in characters:
+           count = lower_text.count(letters)
+           dict[letters] = count
+        #print (dict)
+        sorted_words= sorted(dict.items(), key=lambda item: int(item[1]), reverse=True)
+        #print(sorted_words)
+        for k,v in sorted_words:
+            if k.isalpha():
+                print(f"{k}: {v}")
+        
+
+#count_letters('frankenstein.txt')
